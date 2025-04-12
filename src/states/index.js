f0767ab5997eb08fb8d9fd008a252fb55b9e2254
nouvaldy <110953095+Nouvaldy@users.noreply.github.com>
2025-04-12 03:20:12 +0000

    bikin redux store

--- src/states/index.js
+++ src/states/index.js
@@ -1,3 +1,18 @@
-/**
- * @TODO: Create Redux store
- */
+import { configureStore } from "@reduxjs/toolkit";
+import authUserReducer from "./authUser/reducer";
+import isPreloadReducer from "./isPreload/reducer";
+import talkDetailReducer from "./talkDetail/reducer";
+import talksReducer from "./talks/reducer";
+import usersReducer from "./users/reducer";
+
+const store = configureStore({
+  reducer: {
+    authUser: authUserReducer,
+    isPreload: isPreloadReducer,
+    talkDetail: talkDetailReducer,
+    talks: talksReducer,
+    users: usersReducer,
+  },
+});
+
+export default store;
